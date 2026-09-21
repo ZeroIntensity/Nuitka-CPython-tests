6b169946d16ed4c7b2937a7d2a57f493db15369d
Peter Bierma <zintensitydev@gmail.com>
2026-09-20 20:12:33 -0400

    Skip non-deterministic test in test_listcomps.

--- test/test_listcomps.py
+++ test/test_listcomps.py
@@ -180,7 +180,9 @@ class ListComprehensionTest(unittest.TestCase):
                 code, outputs={"res": [2]}, scopes=["module", "function"])
         self._check_in_scopes(code, raises=NameError, scopes=["class"])
 
-    def test_references___classdict__(self):
+    # Nuitka: The error message here is non-deterministic, which breaks output
+    # comparison.
+    def notest_references___classdict__(self):
         code = """
             class i: [__classdict__ for x in y]
         """
